ac8e1b1004e8c900d2e59183052d330bee512647
implaymo <136201112+implaymo@users.noreply.github.com>
2023-11-01 12:43:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+import requests
+
+MY_LAT = 56.326341
+MY_LONG = -350.480347
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+time_now = datetime.now()
+time_hour = time_now.hour
